52488cedf72cc97c9bd3d8ec36ff9a856d75fccf
Andy Kramer <andy.kramer@ostendis.com>
2023-08-31 11:04:58 +0200

    Add tracking consent state

--- src/GoogleTagManager.php
+++ src/GoogleTagManager.php
@@ -36,11 +36,34 @@ class GoogleTagManager extends Widget
         }
 
         $view = $this->getView();
+        $view->registerJs("window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'analytics_storage': 'denied',
+                'functionality_storage': 'denied',
+                'personalization_storage': 'denied',
+                'security_storage':'denied'
+                }
+            );",View::POS_HEAD);
+
         $view->registerJs("(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','{$this->gtmId}');", View::POS_HEAD);
+
+        $view->registerJs("
+            function consentGranted() {
+                gtag('consent', 'update', {
+                    'ad_storage': 'granted',
+                    'analytics_storage': 'granted',
+                    'functionality_storage': 'granted',
+                    'personalization_storage': 'granted',
+                    'security_storage':'granted'
+                });
+            }",View::POS_HEAD);
+
 
         return Html::tag('noscript', Html::tag('iframe', '', [
             'src'    => 'https://www.googletagmanager.com/ns.html?id=' . $this->gtmId,
